e671fc69cd28565408960ac1aa280ab4d013af27
William Fernando Ramirez Paucar <105876217+wiliferamirez@users.noreply.github.com>
2025-05-31 19:26:48 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,31 +1,21 @@
-import pika
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
 from models.orden import OrdenCompra
+import os
+from dotenv import load_dotenv
 
-# Crear orden de ejemplo
+load_dotenv()
+
+
+CONNECTION_STR = os.getenv("AZURE_SERVICEBUS_CONNECTION_SEND")
+QUEUE_NAME = "items"
+
 orden = OrdenCompra("Romel Gualoto", "Teclado mecánico", "electronica", 2)
 mensaje = orden.to_json()
 
-# Conectar a RabbitMQ
-conexion = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-canal = conexion.channel()
+with ServiceBusClient.from_connection_string(CONNECTION_STR) as client:
+    sender = client.get_queue_sender(queue_name=QUEUE_NAME)
+    with sender:
+        msg = ServiceBusMessage(mensaje)
+        sender.send_messages(msg)
 
-# Crear Exchanges
-canal.exchange_declare(exchange='direct_ordenes', exchange_type='direct')
-canal.exchange_declare(exchange='fanout_reporte', exchange_type='fanout')
-
-# Enviar al direct exchange con routing key 'orden.electronica'
-canal.basic_publish(
-    exchange='direct_ordenes',
-    routing_key='orden.electronica',
-    body=mensaje
-)
-
-# Enviar también al fanout (reporte general)
-canal.basic_publish(
-    exchange='fanout_reporte',
-    routing_key='',
-    body=mensaje
-)
-
-print("Orden enviada a ambos exchanges")
-conexion.close()
+print("✅ Orden enviada a Azure Service Bus")
